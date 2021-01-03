934fe3165fce3a1822ddf6e595bfde0ac35077bd
opeolluwa <adefemih214@yahoo.com>
2021-01-03 17:43:52 +0100

    $

--- SML.js
+++ SML.js
@@ -4,28 +4,34 @@ SML 0.00 ==>
 author :opeolluwa
 date :Dec 27 2020
 */
-
-
+  'use strict';
 let Sml = {
   //split the stiring, capitalize the word then return it
-  capitalize(string) {
-    string = string.trim().split('');
+  _capitalize() {
+    let string = this.trim().split('');
     if (string[0]) {
       string[0] = string[0].toUpperCase();
     }
     return string[0] + string.slice(1).join('');
   },
   //check if a string has a sub string, return bool
-  hasSubStr(subStr, str) {
-    return new RegExp(subStr).test(str);
+  _hasSubStr(subStr) {
+    return new RegExp(subStr).test(this);
+  },
+  //compare two string, accept one parameter
+  _compareTo(string) {
+    return this === string;
+  },
+  _compareToIgnoreCase(string){
+    return this.toLowerCase()=== string.toLowerCase();
+  },
+  _endsWith(string){
+  let thisStr = this.trim().split('');
+  return thisStr.slice(thisStr.length - string.length).join('') ===string;
   }
-
 }
 
- String.prototype.hasSubStr = function(subStr) {
-  return new RegExp(subStr).test(this);
-}
 
-//Object.assign(String.prototype, hasSubStr);
+Object.assign(String.prototype, Sml);
 
-console.log('adeoye'.hasSubStr('o'))
+console.log('adeoye'._endsWith('oye'))
